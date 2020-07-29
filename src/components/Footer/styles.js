5724a5234114fcad83d68dbb7ce569974f6533ea
amanda-mendes12 <61382196+amanda-mendes12@users.noreply.github.com>
2020-07-29 14:13:35 -0300

    first commit

---
+++ src/components/Footer/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+
+export const FooterBase = styled.footer`
+  background: #59394B;
+  border-top: 2px solid #3F3F40;
+  padding-left: 16px;
+  padding-right: 16px;
+  padding-top: 32px;
+  padding-bottom: 32px;
+  color: #f7b0ca;
+  text-align: center;
+  @media (max-width: 800px) {
+    margin-bottom: 50px;
+  }
+`;
